dd9fedcb3fb8b4ccb5375c71c2ae145367f8a958
Al Zilqad <alzilqad@gmail.com>
2020-10-08 19:33:48 +0600

    alignment

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./menu-item.styles.scss";
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div
     className={`${size} menu-item`}
     onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
-    <div 
+    <div
       className="background-image"
       style={{
         backgroundImage: `url(${imageUrl})`,
